d255b0c338a4ad14757c0899980e9c6e14d05a95
Guillaume Lazaro <guillaume.lazaro@gmail.com>
2017-10-24 13:51:56 +0200

    Calculatrice v1.0 avec addition fonctionnelle

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,98 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var secondOperandLabel: UILabel!
+    @IBOutlet weak var operatorLabel: UILabel!
+    
+    var a: Int = 0
+    var b: Int = 0
+    
+    var oppActual: OPP = OPP.add
+    
+    enum OPP {
+        case add
+        case sub
+        case mult
+        case div
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Alpha initalisation for the resultLabel
+        self.resultLabel.alpha = 0
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //Animation Fade In for the resultLabel
+        UIView.animate(withDuration: 2) { 
+            self.resultLabel.alpha = 1
+
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    /*override func viewWilldisappear(_ animated: Bool) {
+        super?viewWilldisappear(<#T##animated: Bool##Bool#>)
+    }*/
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func didPressNumber(_ sender: Any) {
+        
+        guard let button = sender as? UIButton else {
+            return
+        }
+        
+        if resultLabel.text == "0" {
+            resultLabel.text = button.titleLabel?.text
+        } else {
+            resultLabel.text?.append(button.titleLabel?.text ?? "")
+        }
+    }
+    
+    @IBAction func didPressAdd(_ sender: Any) {
+        oppActual = OPP.add
+        if(b>0) {
+            a = Int(resultLabel.text ?? "") ?? 0
+            b = b+a
+        } else {
+            b = Int(resultLabel.text ?? "") ?? 0
+        }
+        secondOperandLabel.text=String(b)
+        resetDisplay()
+    }
+    
+    @IBAction func didPressSub(_ sender: Any) {
+        oppActual = OPP.sub
+    }
+    
+    @IBAction func didPressReset(_ sender: Any) {
+        resetDisplay()
+        secondOperandLabel.text = ""
+    }
+    
+    @IBAction func didPressEqual(_ sender: Any) {
+        if(b>0) {
+            //Add:
+            a=Int(resultLabel.text ?? "") ?? 0
+            secondOperandLabel.text = String(a+b)
+            resetDisplay()
+        }
+    }
+    
+    func resetDisplay() {
+        resultLabel.text = "0"
+    }
 }
 
